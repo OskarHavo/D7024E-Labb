f99314afd4c3bbeeccbf2336aa2f3afd2c8d42b5
WeRiano <williamfors13@gmail.com>
2021-09-02 11:43:51 +0200

    Simple node ping app

--- Hello_comms/node.go
+++ Hello_comms/node.go
@@ -8,79 +8,91 @@ import (
 	"strings"
 )
 
-func HandleConnection(c net.Conn) {
-	fmt.Printf("Serving %s\n", c.RemoteAddr().String())
-	msg, err := bufio.NewReader(c).ReadString('\n')
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	fMsg := strings.TrimSuffix(msg, "\n")
-	fmt.Println("Message \"" + fMsg + "\" was received successfully!. Sending ack ...")
-
-	reply := "Ack!"
-	c.Write([]byte(reply))
-	c.Close()
-}
-
 func CheckAndPrintError(err error) {
 	if err != nil {
 		fmt.Println(err)
 	}
 }
 
-func Rec(port string) {
+func Send() {
+	fmt.Println("Running sender!")
+
 	for {
-		fmt.Println("Running receiver!")
-
-		fmt.Println("Listening to port " + port)
-		con, err := net.Listen("tcp", "localhost:" + port)
-		CheckAndPrintError(err)
-
-		c, err := con.Accept()
-		fmt.Println("Connection accepted!")
-		CheckAndPrintError(err)
-		fmt.Println("Handling connection ...")
-		HandleConnection(c)
-
-		con.Close()
-	}
-}
-
-func Send(port string) {
-	for {
-		fmt.Println("Running sender!")
-
 		var conn net.Conn
 		var err error
-		conn, err = net.Dial("tcp", "localhost:" + port)
-		for err != nil {
+		var port string
+		for {
+			fmt.Println("Enter port to ping: ")
+			rawPort, inputErr := bufio.NewReader(os.Stdin).ReadString('\n')
+			CheckAndPrintError(inputErr)
+			port = strings.Split(rawPort, "\n")[0]
+
+			fmt.Println("Establishing connection to localhost:" + port)
 			conn, err = net.Dial("tcp", "localhost:" + port)
+			if err == nil {
+				break
+			} else {
+				fmt.Println("Could not dial localhost:" + port + ". Try again")
+				fmt.Println(err)
+			}
 		}
-		CheckAndPrintError(err)
 
-		//msg := "Test msg from sender!"
-		// Get msg to send from keyboard input
-		fmt.Println("Enter msg to send: ")
-		msg, err := bufio.NewReader(os.Stdin).ReadString('\n')
-		CheckAndPrintError(err)
-		fmt.Fprintf(conn, msg + "\n") // Send msg
+		fmt.Println("Connection established! Sending ping msg.")
+		conn.Write([]byte("This is a ping msg!" + "\n"))
 
-		reply, _ := bufio.NewReader(conn).ReadString('\n')
-		fmt.Println("Received response: " + reply)
+		reply := make([]byte, 256)
+		conn.Read(reply)
 
+		fReply := strings.Split(string(reply), "\n")
+		if fReply[0] == "Ack!" {
+			fmt.Println("Received ack from localhost:" + port)
+		} else {
+			fmt.Println("Received unrecognized response from localhost: " + port)
+		}
+
+		fmt.Println("Closing connection ...")
 		conn.Close()
 	}
 }
 
+
+func Rec(port string) {
+	fmt.Println("Running receiver!")
+
+	fmt.Println("Listening to port " + port)
+	conn, err := net.Listen("tcp", "localhost:" + port)
+	CheckAndPrintError(err)
+
+	for {
+		c, err := conn.Accept()
+		fmt.Println("Connection from " + c.RemoteAddr().String() + " accepted!")
+		CheckAndPrintError(err)
+
+		msg := make([]byte, 256)
+		c.Read(msg)
+
+		fMsg := strings.Split(string(msg), "\n")
+		if fMsg[0] == "This is a ping msg!" {
+			fmt.Println("Received ping from " + c.RemoteAddr().String())
+		} else {
+			fmt.Println("Received unrecognized msg from " + c.RemoteAddr().String())
+		}
+
+		fmt.Println("Sending Ack ...")
+		reply := "Ack!"
+		c.Write([]byte(reply + "\n"))
+		c.Close()
+	}
+	conn.Close()
+}
+
 func main() {
-	if len(os.Args[1:]) != 2 {
-		fmt.Println("Expected 2 arguments. Got:", len(os.Args[1:]))
+	if len(os.Args[1:]) != 1 {
+		fmt.Println("Expected 1 arguments. Got:", len(os.Args[1:]))
 		os.Exit(-1)
 	}
 
 	args := os.Args[1:]
 	go Rec(args[0])
-	Send(args[1])
+	Send()
 }
