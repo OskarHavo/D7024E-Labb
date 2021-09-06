ad6f41089190e7c63c24c2aa45c78342a2cca9d9
WeRiano <williamfors13@gmail.com>
2021-09-06 15:50:22 +0200

    sprint 0 checkpoint merge

--- Hello_comms/build_containers.bat
+++
@@ -1,9 +0,0 @@
-Rem ECHO OFF
-setlocal enabledelayedexpansion
-
-set c=node
-for /l %%x in (5001, 1, 5005) do (
-    set /A t=%%x-5000
-    set n=%c%!t!
-    docker run -i -t -d --rm --name !n! kadlab %%x
-)
